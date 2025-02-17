855355dc937aa313adcd408c952613b9c74ba182
swamy <mails2swami@gmail.com>
2025-02-17 23:45:22 +0530

    terrafor day-4 practices

---
+++ day-4-state-remove-backend/variables.tf
@@ -0,0 +1,14 @@
+variable "ami_id" {
+    type = string
+    default = "ami-0c50b6f7dc3701ddd"
+  
+}
+variable "key_name" {
+    type = string
+    default = "multicloudwithdevops-2"  
+}
+variable "type" {
+    type = string
+    default = "t2.micro"
+  
+}
